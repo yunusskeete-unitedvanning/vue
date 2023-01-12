8e08025fb67c9455415ba72825bc55ee929f7730
yunusskeete-unitedvanning <yunus.skeete@unitedvanning.com>
2023-01-12 10:51:35 +0000

    setup() and data() methods

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,13 +9,25 @@
 
 export default {
   name: 'HomeView',
+  /*
+  setup() is not reacted - if the value changes, that will not be reflected in the webpage.
+  NOTE: We can make these reactive at a later point
+  */
   setup() {
     console.log('setup')
 
     let name = 'Mario'
     let age = 30
-
+    
     return { name: name, age: age }
+  },
+  /*
+  data() is reactive - if the value changes at any point, this will be reflected in the webpage
+  */
+ data() {
+  return {
+    age: 40
   }
+ }
 }
 </script>
