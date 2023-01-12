88a9be90497ef3d627a61ca734df1f5b79e9e420
yunusskeete-unitedvanning <yunus.skeete@unitedvanning.com>
2023-01-12 10:42:29 +0000

    setup() method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    console.log('setup')
+
+    let name = 'Mario'
+    let age = 30
+
+    return { name: name, age: age }
   }
 }
 </script>
