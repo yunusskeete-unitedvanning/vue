d671c3eb7c950803ec4626b58766ffd481e05cdd
yunusskeete-unitedvanning <yunus.skeete@unitedvanning.com>
2023-01-12 11:00:28 +0000

    handleClick() method for event-driven executions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     Home
     <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
@@ -11,15 +12,20 @@ export default {
   name: 'HomeView',
   /*
   setup() is not reacted - if the value changes, that will not be reflected in the webpage.
-  NOTE: We can make these reactive at a later point
+  IMPORTANT: We HAVE to return variables or methods here to have them accessible inside the template.
+  NOTE: We can make these reactive at a later point.
   */
   setup() {
     console.log('setup')
 
     let name = 'Mario'
     let age = 30
-    
-    return { name: name, age: age }
+
+    const handleClick = () => {
+      console.log('You clicked me')
+    }
+
+    return { name, age, handleClick }
   },
   /*
   data() is reactive - if the value changes at any point, this will be reflected in the webpage
